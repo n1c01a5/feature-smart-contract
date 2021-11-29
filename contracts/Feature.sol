5d026a6af272f732d1f94cb95838e01fc7c40bff
httpcode <blockchaindevweb@gmail.com>
2021-11-29 06:18:29 +0100

    fix: change arguments of comments

--- contracts/Feature.sol
+++ contracts/Feature.sol
@@ -538,9 +538,9 @@ contract Feature is Initializable, NativeMetaTransaction, ChainConstants, Contex
     /** @dev To be emitted when a party pays.
      *  @param _transactionID The index of the transaction.
      *  @param _amount The amount paid.
-     *  @param _party The party that paid.
+     *  @param _receiver the receiver of the transaction amount.
      */
-    event Payment(uint256 indexed _transactionID, uint256 _amount, address _party);
+    event Payment(uint256 indexed _transactionID, uint256 _amount, address _receiver);
 
     /** @dev To be emitted when a sender is refunded.
      *  @param _transactionID The index of the transaction.
